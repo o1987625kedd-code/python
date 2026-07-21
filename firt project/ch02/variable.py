520eec35af663973f05423c550743f93b16a4384
o1987625kedd-code <o1987625kedd@gmail.com>
2026-07-21 14:55:10 +0800

    variable

--- firt project/ch02/variable.py
+++ firt project/ch02/variable.py
@@ -21,3 +21,11 @@ print(int("12")*4)
 b = False
 print(type(b))
 
+f,i = 1.2345,12345
+print(type(f))
+f2=float(i)
+print(f2)
+print(float.is_integer(f))
+print(float.is_integer(f2))
+print(round(f,2))
+print(round(f))
