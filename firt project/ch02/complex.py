68f5b7f5ccf9a730fa0a48cbef9cffd501aab4cf
o1987625kedd-code <o1987625kedd@gmail.com>
2026-07-21 11:29:55 +0800

    first commit

---
+++ firt project/ch02/complex.py
@@ -0,0 +1,3 @@
+c = 3+4j
+print(c.real) 
+print(c.imag) 
---
+++ firt project/ch02/str.py
@@ -0,0 +1,9 @@
+print('python')
+print("3.8")
+print("That's a book.")
+print('"Hi!"says Jack.')
+a = "0123456789"
+print(a[0])
+print(a[1])
+print("python"[0])
+print("python"[1])
