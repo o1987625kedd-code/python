eb955fffada0e400c67f3ae952b77dc5e98fe3d8
o1987625kedd-code <o1987625kedd@gmail.com>
2026-07-21 14:31:34 +0800

    variable

---
+++ firt project/ch02/variable.py
@@ -0,0 +1,19 @@
+yesNo="y"
+pssScore,maxScore,minScore = 85,100,0
+power1 = power2 = 100
+total = 1.23456E+6
+
+print(yesNo)
+print(pssScore,maxScore,minScore)
+print(power1,power2)
+print(total)
+#del total
+print(total)
+
+print(type(12))
+print(bin(12))
+print(oct(12))
+print(hex(12))
+print("12"*4)
+print(type("12"))
+print(int("12")*4)
