c81d4ca6becb8e056f7777383542aab28b0cd337
o1987625kedd-code <o1987625kedd@gmail.com>
2026-07-24 16:12:51 +0800

    0724-3

--- firt project/ch02/logical.py
+++ firt project/ch02/logical.py
@@ -3,4 +3,19 @@ print("台北","台中","台南",sep = ",")
 print("%d"%(12345))
  """
 print("%d"%(12345))
- 
+print("1234567890!\a") 
+print("1234567890!\n") 
+print("123\r456789")
+print("123\t4567")
+print("12\\3\'45\"67")
+print("汽水2打24瓶")
+print("汽水%d打24瓶"%2)
+print("%s%d打%d瓶"%("汽水",2,24))
+S1 = "GOOD""GOOD""GOOD" 
+print(S1)
+S1 = "Python 基礎必修課"
+print(S1[3])
+print(S1[-2])
+
+S = "PYTHON 基礎必修課"
+print(S[::-2])
---
+++ firt project/ch03/str2num.py
@@ -0,0 +1,14 @@
+# s1 , s2 = "123" , "12.34"
+# print(int(s1) , type(int(s1)))
+# print(float(s2),
+# user_name = input("請輸入姓名：")
+# age = int(input("請輸入年齡："))
+# print("姓名：%s\t 年齡：%d歲"%(user_name,age))
+s1 = "電車月票"
+s2 = 1280
+print("項目:{0} 金額：{1}元".format(s1,s2))
+
+print("{:.2f}".format(0.6666))
+
+
+               
